472dfc9b9ca24b48d9d62f1c23b7236f91c64dd0
Megavardhini.P <megavardhini1958@gmail.com>
2024-09-21 18:57:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import connectDB from './Database/config.js';
 import bodyParser from 'body-parser';
-import exerciseRoutes from './src/Routes/exerciseRoutes.js'
-import goalRoutes from './src/Routes/goalRoutes.js'
-import authRoutes from './src/Routes/authRoutes.js'
-import adminRoutes from './src/Routes/adminRoutes.js'
-import foodRoutes from './src/Routes/FoodRoutes.js'
+import exerciseRoutes from './src/Routes/exerciseRoutes.js';
+import goalRoutes from './src/Routes/goalRoutes.js';
+import authRoutes from './src/Routes/authRoutes.js';
+import adminRoutes from './src/Routes/adminRoutes.js';
+import foodRoutes from './src/Routes/FoodRoutes.js';
 
-dotenv.config()
+dotenv.config();
 const app = express();
-connectDB()
-const port=process.env.PORT
- app.use(cors())
-// app.use(cors({
-//     origin: ['https://bright-begonia-db7907.netlify.app'], // Include all necessary origins
-//     credentials: true, // Enable sending cookies
-// }));
+connectDB();
+
+const port = process.env.PORT ;
 const corsOptions = {
   origin: 'https://bright-begonia-db7907.netlify.app', // Your frontend URL
   credentials: true, // Allow cookies to be sent
 };
 
 app.use(cors(corsOptions));
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(bodyParser.json());
- app.use('/api/food',foodRoutes)
-app.use('/api/auth',authRoutes)
-app.use('/api/admin',adminRoutes)
-app.use('/api/exercises', exerciseRoutes)
-app.use('/api/goals',goalRoutes)
-app.get('/',(req,res)=>{
-    res.send(`<h1>Welcome to Health and Wellness</h1>`)
-})
+
+// Define your routes
+app.use('/api/food', foodRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/exercises', exerciseRoutes);
+app.use('/api/goals', goalRoutes);
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Welcome to Health and Wellness</h1>`);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
-
-
-
-
+  console.log(`Server is running on port ${port}`);
+});
