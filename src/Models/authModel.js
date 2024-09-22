244d40e34aa232540942637e693fab1cd57cc095
Megavardhini.P <megavardhini1958@gmail.com>
2024-09-22 09:41:53 +0530

    Update authModel.js

--- src/Models/authModel.js
+++ src/Models/authModel.js
@@ -1,22 +1,29 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: { type: String, required: true, index: true },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+    match: /.+\@.+\..+/ // Basic email format validation
+  },
+  password: { 
+    type: String, 
+    required: true, 
+    minlength: 6 // Ensuring a minimum password length
+  },
   profilePicture: { type: String },
   fitnessGoals: { type: Map, of: Number },
   nutritionPreferences: { type: Map, of: String },
-  mentalHealthLogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'MoodLog' }],
   age: { type: Number, required: true },
   height: { type: Number, required: true },
   weight: { type: Number, required: true },
-  location: { type: String, required: true },
+  location: { type: String, required: true, index: true },
   randomString: String,
   expirationTimestamp: Number,
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  steps: { type: Number, default: 0 }, // Default steps count
-  dailyGoal: { type: Number, default: 10000 }, // Default daily step goal
+ 
 });
 
 export default mongoose.model('User', UserSchema);
